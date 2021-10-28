e799a296fe68058d0dd195712d05b9638993ecb5
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-10-28 06:02:32 +0000

    linq

--- main.cs
+++ main.cs
@@ -19,7 +19,6 @@ class Eu{
         this.datum  = s[1];
         this.ev     = s[1].Substring(0,4);
         this.honap  = s[1].Substring(5,2);
-
     }
 }
 
@@ -37,15 +36,26 @@ class Program{
     Console.WriteLine($"3. feladat: EU tagállamainak száma: {lista.Count} db");
     
 // 4.feladat: 2007-ben ? ország csatlakozott.
-    var darab = (from sor in lista where sor.ev == "2007" select sor).Count();
+    var darab = (
+        from sor in lista 
+        where sor.ev == "2007" select sor
+        ).Count();
     Console.WriteLine($"4. feladat: 2007-ben {darab} ország csatlakozott.");
 
 // 5. feladat: Magyarország csatlakozásának dátuma:
-    var mo = (from sor in lista where sor.orszag == "Magyarország" select sor.datum).Last();
+    var mo = (
+        from sor in lista 
+        where sor.orszag == "Magyarország" 
+        select sor.datum
+        ).Last();
     Console.WriteLine($"5. feladat: Magyarország csatlakozásának dátuma: {mo}");
 
 // 6. feladat: Májusban ? csatlakozás.
-    var csatlakozas05 = (from sor in lista where sor.honap == "05" select sor); 
+    var csatlakozas05 = (
+        from sor in lista 
+        where sor.honap == "05" 
+        select sor
+        ); 
     if (csatlakozas05.Any()){
         Console.WriteLine($"6. feladat: Májusban volt csatlakozás.");
     }
@@ -54,12 +64,19 @@ class Program{
     }
     
 // 7. Utoljára csatlakozott tagállam 
-    var utolso_csatlakozas = (from sor in lista orderby sor.datum select sor).Last();
+    var utolso_csatlakozas = (
+        from sor in lista 
+        orderby sor.datum 
+        select sor
+        ).Last();
     Console.WriteLine($"7. feladat: Legutoljára csatlakozott ország: {utolso_csatlakozas.orszag}");
 
 // 8. feladat: évenkénti statisztika
 
-    var statisztika = from sor in lista group sor by sor.ev;
+    var statisztika = (
+        from sor in lista 
+        group sor by sor.ev
+        );
     Console.WriteLine($"8. feladat: Statisztika");
     foreach(var q in statisztika){
         Console.WriteLine($"        {q.Key} - {q.Count()} ország");
